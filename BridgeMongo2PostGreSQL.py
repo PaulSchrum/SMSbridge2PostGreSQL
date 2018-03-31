462a6de7ed87495029fdeac5077dc03c1ac2b691
Paul Schrum <ptschrum@ncsu.edu>
2018-03-31 11:04:10 -0400

    Work In Progress.

--- BridgeMongo2PostGreSQL.py
+++ BridgeMongo2PostGreSQL.py
@@ -6,13 +6,14 @@ with data from them.
 '''
 print 'started'
 import arcpy
+import os
 print 'imports complete'
 
 mxd = None
 mxdPath = r'D:\NCSU\201801 Spring 2018\GIS 590 Capstone Project\Sandbox\attempt 2\arcmapStuff\Testbed.mxd'
 lyrStationsName = 'Stations'
-tblGages = 'Gages'
-tblStatuses = 'Statuses'
+tblGages = 'tbl_gages'
+tblStatuses = 'tbl_statuses'
 
 stationRequiredFields = [('stationName', 'STRING', '31'),
                           ('projectName', 'STRING', '31'),
@@ -20,7 +21,7 @@ stationRequiredFields = [('stationName', 'STRING', '31'),
 
 gagesRequiredFields = [('gageId', 'STRING', '31'),
                        ('gageModelName', 'STRING', '31'),
-                       ('gageModelName', 'STRING', '31'),
+                       ('stationName', 'STRING', '31'),
                        ('assigned', 'STRING', '31'),
                        ('comments', 'STRING', '63'),
                        ('createdAt', 'DATE', ''),
@@ -52,8 +53,24 @@ def getLayerIndex(lyrList, layerName):
     lyrNames = [L.name for L in lyrList]
     return lyrNames.index(layerName)
 
+def getLayersDict(mxd):
+    returnDict = {}
+    layers = arcpy.mapping.ListLayers(mxd)
+    for lyr in layers:
+        returnDict[lyr.name] = lyr
+    return returnDict
+
+def getGDBfromDataSource(layer):
+    delim = '\\'
+
 def ensureTableLayerHasFields(layerObj, fieldList):
-    existingFields = arcpy.ListFields(layerObj.dataSource)
+    myDataSource = layerObj
+    aType = type(layerObj)
+    useless = 'hi'
+    bType = type(useless)
+    if not(isinstance(layerObj, basestring)):
+        myDataSource = layerObj.dataSource
+    existingFields = arcpy.ListFields(myDataSource)
     names = [f.name for f in existingFields]
     for requiredField in fieldList:
         fieldName = requiredField[0]
@@ -63,19 +80,39 @@ def ensureTableLayerHasFields(layerObj, fieldList):
             arcpy.AddField_management(layerObj, fieldName, fieldType,
                                       field_length=fLen)
 
-    print existingFields
+def initializeTables(mxd):
+    lyrDict = getLayersDict(mxd)
+    lyrStations = lyrDict[lyrStationsName]
+    workingGdb = os.path.dirname(lyrStations.dataSource)
+
+    try:
+        ensureTableLayerHasFields(lyrStations, stationRequiredFields)
+    except:
+        pass
+
+    try:
+        fullPath = os.path.join(workingGdb, tblGages)
+        ensureTableLayerHasFields(fullPath, gagesRequiredFields)
+    except:
+        pass
+
+    try:
+        fullPath = os.path.join(workingGdb, tblStatuses)
+        ensureTableLayerHasFields(fullPath, statusesRequiredFields)
+    except:
+        pass
+
+    print 'tables initialized'
+
 
 if __name__ == '__main__':
     mxd = arcpy.mapping.MapDocument(mxdPath)
-    layers = arcpy.mapping.ListLayers(mxd)
-    lyrIndex = getLayerIndex(layers, lyrStationsName)
-    lyrStations = layers[lyrIndex]
-    print lyrStations.name
+    initializeTables(mxd)
+
     # print arcpy.GetCount_management(lyrStations), 'before deleteAllRows'
     # deleteAllRows(lyrStations)
     # print arcpy.GetCount_management(lyrStations), 'after deleteAllRows'
 
-    ensureTableLayerHasFields(lyrStations, stationRequiredFields)
     print 'Done'
 
 
