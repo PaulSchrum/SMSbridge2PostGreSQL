ab55f557abfe4199a76b7dbc6713283e0656f331
Paul Schrum <ptschrum@ncsu.edu>
2018-04-03 18:09:10 -0400

    Added module for Station class.

---
+++ station.py
@@ -0,0 +1,44 @@
+
+import os, json
+
+class Station():
+    def __init__(self, rowStr):
+        aDict = json.loads(rowStr)
+        self.id = aDict['_id']
+        self.project = aDict['project']
+        self.projectId = aDict['projectId']
+        self.stationName = aDict['stationName']
+        self.stationNumber = aDict['stationNumber']
+        self.stationLoc = aDict['stationLoc']
+        self.owner = aDict['owner']
+        self.description = aDict['description']
+        self.status = aDict['status']
+        self.lat = float(aDict['lat'])
+        self.lon = float(aDict['lon'])
+        self.waterDepth = aDict['waterDepth']
+        self.createdAt = aDict['createdAt']
+        self.createdBy = aDict['createdBy']
+
+def GetAllStationsDict(pathFN):
+    returnDict = {}
+    with open(pathFN, 'r') as theFile:
+        allLines = theFile.readlines()
+
+    for aRow in allLines:
+        aStation = Station(aRow)
+        returnDict[aStation.id] = aStation
+
+    return returnDict
+
+
+
+if __name__ == '__main__':
+    testDir = 'Data/FRFdata'
+    cwd = os.getcwd()
+    testFullPath = os.path.join(cwd, testDir)
+    stationsFile = os.path.join(testFullPath, 'stations.json')
+
+    allStationsDict = GetAllStationsDict(stationsFile)
+    print allStationsDict
+
+
