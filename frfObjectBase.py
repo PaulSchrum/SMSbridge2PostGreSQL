49475b8773cf36e6f75de55776bdf56c5146ea0d
Paul Schrum <ptschrum@ncsu.edu>
2018-04-04 16:21:14 -0400

    Added function comments to _cleanUpNAs().

--- frfObjectBase.py
+++ frfObjectBase.py
@@ -2,6 +2,13 @@
 
 class FrfObjectBase:
     def _cleanUpNAs(self):
+        '''
+        The FRF data has strings of 'N/A' where the data is not available.
+        For putting these into the PostGres database, this method changes all
+        attributes with 'N/A' to be None. This should only be called from a
+        child classes __init__ method at the end of it.
+        :return: None
+        '''
         for aKey in self.__dict__.keys():
             if self.__dict__[aKey] == 'N/A':
                 self.__dict__[aKey] = None
