1e47e80f678cbb5f95fc50b9212161fb441670e7
Paul Schrum <ptschrum@ncsu.edu>
2018-04-13 10:47:11 -0400

    Major changes, but Work in Progress only. Added capability to read MongeDB dump and populate a feature class with the values (stations only). Update Existing Row does not quite work.

--- BridgeMongo2PostGreSQL.py
+++ BridgeMongo2PostGreSQL.py
@@ -6,9 +6,12 @@ with data from them.
 '''
 print 'started'
 import arcpy
-import os
+import os, json
 from station import Station
+from station import requiredFields as staRequiredFields
+from station import getStationsFieldList as staGetStationsFieldList
 from gage import Gage
+from gage import requiredFields as gageRequiredFields
 print 'imports complete'
 
 mxd = None
@@ -16,17 +19,6 @@ lyrStationsName = 'Stations'
 tblGages = 'tbl_gages'
 tblStatuses = 'tbl_statuses'
 
-stationRequiredFields = [('stationName', 'STRING', '31'),
-                          ('projectName', 'STRING', '31'),
-                          ('WaterDepth', 'SINGLE', '')]
-
-gagesRequiredFields = [('gageId', 'STRING', '31'),
-                       ('gageModelName', 'STRING', '31'),
-                       ('stationName', 'STRING', '31'),
-                       ('assigned', 'STRING', '31'),
-                       ('comments', 'STRING', '63'),
-                       ('createdAt', 'DATE', ''),
-                       ('title', 'STRING', '31')]
 
 def _deleteAllFields_development(layerObj, shouldRun=False):
     '''
@@ -49,10 +41,13 @@ def _deleteAllFields_development(layerObj, shouldRun=False):
         lst = arcpy.ListFields(layerObj.dataSource)
     if len(lst) == 0:
         return None
-    if len(lst) == 0:
-        return None
 
     fieldList = [fld.name.upper() for fld in lst]
+    fieldList = [fldName for fldName in fieldList
+                 if not (fldName == 'OBJECTID' or
+                    fldName == 'SHAPE')]
+    if len(fieldList) == 0:
+        return None
     arcpy.DeleteField_management(layerObj, fieldList)
 
 
@@ -105,7 +100,7 @@ def initializeTables(mxd):
     workingGdb = os.path.dirname(lyrStations.dataSource)
 
     try:
-        ensureTableLayerHasFields(lyrStations, stationRequiredFields)
+        ensureTableLayerHasFields(lyrStations, staRequiredFields)
     except:
         pass
 
@@ -131,23 +126,116 @@ def _getFirstOrDefault(aCollection):
     # from https://stackoverflow.com/a/365934/1339950
     return next(iter(aCollection), None)
 
+def ensureAllColumns(layerObj, theType=''):
+    if theType == '': return
+    if 'STATIO' in theType.upper():
+        ensureTableLayerHasFields(layerObj, staRequiredFields)
+    elif 'GAGE' in theType.upper():
+        ensureTableLayerHasFields(layerObj, gageRequiredFields)
+
+def readStationsJson(stationsFileName):
+    stationsDict = {}
+    with open(stationsFileName, 'r') as inFile:
+        allLines = inFile.readlines()
+        for aLine in allLines:
+            # rowDict = json.loads(aLine)
+            aStation = Station(aLine)
+            stationsDict[aStation._id] = aStation
+
+    return stationsDict
+
+def getAllIds(layerObj):
+    '''
+    For the given layer object, return a set of all _id's in the
+    Attribute Table underlying the layer.
+    :param layerObj:
+    :return: set of all _id values
+    '''
+    idSet = set()
+    with arcpy.da.SearchCursor(layerObj, ['_id']) as cursor:
+        for row in cursor:
+            idSet.add(row[0])
+    del cursor
+    return idSet
+
+def appendOrUpdateStations(stationsLayer, stationsDict):
+    existingStationIds = getAllIds(stationsLayer)
+
+    incomingStationIDset = set(stationsDict.keys())
+    newStationsIDset = incomingStationIDset - existingStationIds
+    updateableStations = existingStationIds.intersection(incomingStationIDset)
+
+    fieldList = set(staGetStationsFieldList())
+    # remove immutable fields - Update works only with
+    #    mutable fields
+    fListMutables = set(fieldList)
+    fListMutables = fListMutables.difference(set(['SHAPE@XY', 'lat', 'lon']))
+    fListMutables = list(fListMutables)
+
+    whereStatement = '_id IN ('
+    # update the existing stations of the Layer
+    if len(updateableStations) > 0:
+
+        # Only get rows where station id is in updateableStations
+        for idx, aStaId in enumerate(updateableStations):
+            if idx > 0:
+                whereStatement = whereStatement + ","
+            whereStatement = whereStatement + "'" + aStaId + "'"
+        whereStatement = whereStatement + ')'
+
+        with arcpy.da.UpdateCursor(stationsLayer, fListMutables, whereStatement) as cursor:
+            idIndex = fListMutables.index('_id')
+            for aRow in cursor:
+                staId = aRow[idIndex]
+                newStationData = stationsDict[staId]
+                for fName in fListMutables:
+                    rowIdx = fListMutables.index(fName)
+                    newValue = newStationData.__dict__[fName]
+                    aRow[rowIdx] = newValue
+                cursor.updateRow(aRow)
+                i = 0
+        del cursor
+
+    # write the new stations to the Layer
+    with arcpy.da.InsertCursor(stationsLayer, fieldList) as cursor:
+        while len(newStationsIDset) > 0:
+            stationID = newStationsIDset.pop()
+            aStation = stationsDict[stationID]
+            rowList = aStation.createOrUpdateRow()
+            cursor.insertRow(rowList)
+    del cursor
+
+    i = 0
+
+def readAllJsonDumps(stationsLayer, stationsName): #, gagesName):
+    stationsDict = readStationsJson(stationsName)
+    appendOrUpdateStations(stationsLayer, stationsDict)
+
+    i = 0
+
 if __name__ == '__main__':
     testDir = 'Data/arcmapStuff'
     cwd = os.getcwd()
     testFullPath = os.path.join(cwd, testDir)
     mapFileName = os.path.join(testFullPath, 'testBed.mxd')
+    frfDir = os.path.join(cwd, 'Data/FRFdata')
+    stationsFname = os.path.join(frfDir, 'stations3.json')
+    stationsJsonDump = os.path.join(testFullPath, stationsFname)
 
     mxd = arcpy.mapping.MapDocument(mapFileName)
     stationsLayer = _getFirstOrDefault(
         [L for L in arcpy.mapping.ListLayers(mxd)
             if L.name == 'Station'])
-    temp = arcpy.ListFields(stationsLayer.dataSource)
-    _deleteAllFields_development(stationsLayer, shouldRun=True)
-    # initializeTables(mxd)
+    # _deleteAllFields_development(stationsLayer, shouldRun=True)
+    stationFields = arcpy.ListFields(stationsLayer.dataSource)
+    if len(stationFields) <= 2:
+        ensureTableLayerHasFields(stationsLayer, staRequiredFields)
+    # deleteAllRows(stationsLayer)
+    readAllJsonDumps(stationsLayer, stationsJsonDump)
 
-    # print arcpy.GetCount_management(lyrStations), 'before deleteAllRows'
-    # deleteAllRows(lyrStations)
-    # print arcpy.GetCount_management(lyrStations), 'after deleteAllRows'
+    # print arcpy.GetCount_management(stationsLayer), 'before deleteAllRows'
+    # deleteAllRows(stationsLayer)
+    # print arcpy.GetCount_management(stationsLayer), 'after deleteAllRows'
 
     print 'Done'
 
