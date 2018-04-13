5da366f06aca0733949dc6d2551e0b474d9f2bb2
Paul Schrum <ptschrum@ncsu.edu>
2018-04-13 13:41:04 -0400

    Modified comments to reflect what is happening a little better.

--- BridgeMongo2PostGreSQL.py
+++ BridgeMongo2PostGreSQL.py
@@ -1,8 +1,10 @@
 '''
-Accepts data from a ??? file consisting of multiple JSON objects where
-the ??? file was generated from MongoDB via a data dump.
+Accepts data from .json files consisting of multiple JSON objects where
+the .json file was generated from MongoDB via a data dump.
 Parses the JSON objects and populates a feature class and two tables
 with data from them.
+
+The feature store may be of either kind, gdb or PostGres database.
 '''
 print 'started'
 import arcpy
