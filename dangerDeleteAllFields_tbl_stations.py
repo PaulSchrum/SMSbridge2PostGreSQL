145a9058cf2f947bb932db0a0e7db89e0fef5b52
Paul Schrum <ptschrum@ncsu.edu>
2018-04-19 10:56:19 -0400

    Minor: Added alternative lines of code depending on what machine is running it.

--- dangerDeleteAllFields_tbl_stations.py
+++ dangerDeleteAllFields_tbl_stations.py
@@ -3,10 +3,12 @@ Intended to be run from the Python window of an arcMap session.
 Only.
 '''
 import sys, arcpy
-mxd = arcpy.mapping.MapDocument('CURRENT')
+mxd = arcpy.mapping.MapDocument(r"D:\SourceModules\Python\SMSbridge2PostGreSQL\Data\arcmapStuff\Testbed.mxd")
+# mxd = arcpy.mapping.MapDocument("CURRENT")
 layrs = arcpy.mapping.ListLayers(mxd)
 staLyr = layrs[0]
-sys.path.append("C:\SourceCode\Python\SMSbridge2PostGreSQL")
-# from BridgeMongo2PostGreSQL import deleteAllFields
-# deleteAllFields(staLyr, True)
+sys.path.append(r"D:\SourceModules\Python\SMSbridge2PostGreSQL\BridgeMongo2PostGreSQL.py")
+# sys.path.append(r"C:\SourceCode\Python\SMSbridge2PostGreSQL\BridgeMongo2PostGreSQL.py")
+from BridgeMongo2PostGreSQL import deleteAllFields
+deleteAllFields(staLyr, True)
 
